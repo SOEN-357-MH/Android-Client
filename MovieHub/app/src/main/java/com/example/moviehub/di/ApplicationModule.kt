ceef02f7d88eced06ec4fa8828c76c72b4160c48
John <johnab4444@gmail.com>
2021-03-13 21:37:45 -0500

    created app module to hold retrofit functions

---
+++ MovieHub/app/src/main/java/com/example/moviehub/di/ApplicationModule.kt
@@ -0,0 +1,71 @@
+package com.example.moviehub.di
+
+import com.example.moviehub.BuildConfig
+import com.example.moviehub.misc.Constants
+import com.example.moviehub.repository.DefaultMainRepository
+import com.example.moviehub.repository.MainRepository
+import com.example.moviehub.retrofit.ApiService
+import com.example.moviehub.retrofit.OAuthInterceptor
+import com.example.moviehub.utils.DispatcherProvider
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class ApplicationModule {
+
+    @Provides
+    fun provideBaseUrl() = Constants.BASE_URL
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient() = if(BuildConfig.DEBUG){
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+        OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .addInterceptor(OAuthInterceptor())
+            .build()
+    }else OkHttpClient
+        .Builder()
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient, BASE_URL: String): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): ApiService = retrofit.create(ApiService::class.java)
+
+    @Provides
+    @Singleton
+    fun provideMainRepository(api: ApiService): MainRepository = DefaultMainRepository(api)
+
+    @Provides
+    @Singleton
+    fun provideDispatchers(): DispatcherProvider = object : DispatcherProvider {
+        override val main: CoroutineDispatcher
+            get() = Dispatchers.Main
+        override val io: CoroutineDispatcher
+            get() = Dispatchers.IO
+        override val default: CoroutineDispatcher
+            get() = Dispatchers.Default
+        override val unconfined: CoroutineDispatcher
+            get() = Dispatchers.Unconfined
+    }
+}
