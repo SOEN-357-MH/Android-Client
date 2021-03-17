71f9e6fd9694b0be0d944bb08e4caef522c42df3
John <johnab4444@gmail.com>
2021-03-17 14:55:02 -0400

    added login api

--- MovieHub/app/src/main/java/com/example/moviehub/repository/MainRepository.kt
+++ MovieHub/app/src/main/java/com/example/moviehub/repository/MainRepository.kt
@@ -1,10 +1,13 @@
 package com.example.moviehub.repository
 
+import com.example.moviehub.models.LoginBody
 import com.example.moviehub.models.UserBody
 import com.example.moviehub.utils.Resource
 import okhttp3.ResponseBody
 
 interface MainRepository {
+
+    suspend fun loginUser(loginBody: LoginBody): Resource<ResponseBody>
 
     suspend fun registerUser(userBody: UserBody): Resource<ResponseBody>
 }
