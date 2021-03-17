80ea41ea139634b281a575e064c111d0c8c3470c
John <johnab4444@gmail.com>
2021-03-16 21:29:08 -0400

    created register api

--- MovieHub/app/src/main/java/com/example/moviehub/repository/MainRepository.kt
+++ MovieHub/app/src/main/java/com/example/moviehub/repository/MainRepository.kt
@@ -1,4 +1,10 @@
 package com.example.moviehub.repository
 
+import com.example.moviehub.models.UserBody
+import com.example.moviehub.utils.Resource
+import okhttp3.ResponseBody
+
 interface MainRepository {
+
+    suspend fun registerUser(userBody: UserBody): Resource<ResponseBody>
 }
--- MovieHub/app/src/main/java/com/example/moviehub/retrofit/ApiService.kt
+++ MovieHub/app/src/main/java/com/example/moviehub/retrofit/ApiService.kt
@@ -1,4 +1,13 @@
 package com.example.moviehub.retrofit
 
+import com.example.moviehub.models.UserBody
+import okhttp3.ResponseBody
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.PUT
+
 interface ApiService {
+
+    @PUT("account/addUser")
+    suspend fun registerUser(@Body userBody: UserBody): Response<ResponseBody>
 }
