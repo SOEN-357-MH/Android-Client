c41b4e6faa87f88902e40e47e9d63e3b85b04908
JC <jmanikis@icloud.com>
2021-03-19 00:04:08 -0400

    added Theme, Colors, Home Frag

---
+++ MovieHub/app/src/main/java/com/example/moviehub/adapters/MainRecyclerAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.moviehub.adapters
+
+import android.content.Context
+import android.util.Log
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.moviehub.databinding.FragmentHomeBinding
+import com.example.moviehub.models.AllCategory
+
+//class MainRecyclerAdapter(
+//    private val context: Context,
+//    private val allCategory: List<AllCategory>) :
+//    RecyclerView.Adapter<MainRecyclerAdapter.MainViewHolder>() {
+//
+//    private val TAG = "MainAdapter"
+//
+//
+//    inner class MainViewHolder(): RecyclerView.ViewHolder(binding.root){
+//
+//    }
+//
+//
+//
+//    override fun onCreateViewHolder(
+//        parent: ViewGroup,
+//        viewType: Int
+//    ): MainRecyclerAdapter.MainViewHolder {
+//        return MainViewHolder()
+//    }
+//
+//    override fun onBindViewHolder(holder: MainRecyclerAdapter.MainViewHolder, position: Int) {
+//
+//    }
+//
+//    override fun getItemCount(): Int {
+//       return allCategory.size
+//    }
+//
+//
+//}
