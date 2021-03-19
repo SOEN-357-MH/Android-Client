87f2be952f96d383b5e34627856d4e7b280772cf
JC <jmanikis@icloud.com>
2021-03-19 14:03:52 -0400

    added template for movie tinder interface

---
+++ MovieHub/app/src/main/java/com/example/moviehub/fragments/MovieTinderFragment.kt
@@ -0,0 +1,93 @@
+package com.example.moviehub.fragments
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import com.example.moviehub.R
+import com.example.moviehub.databinding.FragmentMovieTinderBinding
+import com.lorentzos.flingswipe.SwipeFlingAdapterView
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MovieTinderFragment : Fragment() {
+
+    private var _binding: FragmentMovieTinderBinding? = null
+    private val binding get() = _binding!!
+
+
+    private var al = ArrayList<String>()
+    private var arrayAdapter: ArrayAdapter<String>? = null
+    private var i = 0
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        al.add("php")
+        al.add("c")
+        al.add("python")
+        al.add("java")
+
+
+        //choose your favorite adapter
+        arrayAdapter = ArrayAdapter(requireContext(), R.layout.movie_tinder_item, R.id.helloText, al)
+
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMovieTinderBinding.inflate(inflater, container, false)
+
+        //set the listener and the adapter
+        binding.frame.adapter = arrayAdapter
+        binding.frame.setFlingListener(object : SwipeFlingAdapterView.onFlingListener {
+            override fun removeFirstObjectInAdapter() {
+                // this is the simplest way to delete an object from the Adapter (/AdapterView)
+                Log.d("LIST", "removed object!")
+                al.removeAt(0)
+                arrayAdapter?.notifyDataSetChanged()
+            }
+
+            override fun onLeftCardExit(p0: Any?) {
+                //Do something on the left!
+                //You also have access to the original object.
+                //If you want to use it just cast it (String) dataObject
+                Toast.makeText(requireContext(), "Left!", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onRightCardExit(p0: Any?) {
+                Toast.makeText(requireContext(), "Right!", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onAdapterAboutToEmpty(p0: Int) {
+                // Ask for more data here
+                al.add("XML $i")
+                arrayAdapter?.notifyDataSetChanged()
+                Log.d("LIST", "notified")
+                i++
+            }
+
+            override fun onScroll(p0: Float) {
+
+            }
+
+        })
+
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
