31b8af493b30a4e43c7487af240b4769bca08b4a
JC <jmanikis@icloud.com>
2021-03-22 13:57:55 -0400

    added shared element transitions

---
+++ MovieHub/app/src/main/java/com/example/moviehub/fragments/MovieDetailsFragment.kt
@@ -0,0 +1,43 @@
+package com.example.moviehub.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import coil.load
+import com.example.moviehub.databinding.FragmentMovieDetailsBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MovieDetailsFragment : Fragment() {
+
+    private var _binding: FragmentMovieDetailsBinding? = null
+    private val binding get() = _binding!!
+
+    private val args : MovieDetailsFragmentArgs by navArgs()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMovieDetailsBinding.inflate(inflater,container, false)
+
+        binding.detailMovieImg.load(args.mediaBody.poster_path)
+
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
