bdeb8cd6d0227a6ebcb0658e0f970437b4deb874
JC <jmanikis@icloud.com>
2021-03-22 17:35:24 -0400

    added recycler view for logos in movie details

--- MovieHub/app/src/main/java/com/example/moviehub/fragments/MovieDetailsFragment.kt
+++ MovieHub/app/src/main/java/com/example/moviehub/fragments/MovieDetailsFragment.kt
@@ -8,7 +8,9 @@ import android.view.ViewGroup
 import androidx.core.view.ViewCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
 import coil.load
+import com.example.moviehub.adapters.DetailsLogoAdapter
 import com.example.moviehub.databinding.FragmentMovieDetailsBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,6 +21,8 @@ class MovieDetailsFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val args : MovieDetailsFragmentArgs by navArgs()
+
+    private var logoAdapter = DetailsLogoAdapter(ArrayList())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,8 +44,9 @@ class MovieDetailsFragment : Fragment() {
 
         args.mediaBody.providers?.results?.let {
             it.CA?.flatrate?.let { providers ->
-                binding.dStreamingService.text = args.mediaBody.providers?.results?.CA?.flatrate!![0].provider_name
-                binding.dStreamingLogo.load(args.mediaBody.providers?.results?.CA?.flatrate!![0].logo_path)
+                logoAdapter = DetailsLogoAdapter(args.mediaBody.providers?.results?.CA?.flatrate!!)
+                binding.dLogosRV.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
+                binding.dLogosRV.adapter = logoAdapter
             }
         }
 
