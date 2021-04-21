bd393750827eecd571e9bc9e1cbf380e0528a43c
JC <jmanikis@icloud.com>
2021-04-21 19:11:01 -0400

    added rv and watchlist items

--- MovieHub/app/src/main/java/com/example/moviehub/adapters/MainRecyclerAdapter.kt
+++ MovieHub/app/src/main/java/com/example/moviehub/adapters/MainRecyclerAdapter.kt
@@ -9,30 +9,40 @@ import com.example.moviehub.databinding.MovieRecyclerRowItemBinding
 import com.example.moviehub.models.AllCategory
 
 
-
 class MainRecyclerAdapter(
     private val context: Context,
-    private val allCategory: ArrayList<AllCategory>) :
+    private val allCategory: ArrayList<AllCategory>
+) :
     RecyclerView.Adapter<MainRecyclerAdapter.MainViewHolder>() {
 
     private var movieItemAdapter: MovieItemAdapter? = null
 
-    inner class MainViewHolder(val binding: MovieRecyclerRowItemBinding): RecyclerView.ViewHolder(binding.root){
+    inner class MainViewHolder(val binding: MovieRecyclerRowItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(categoryItem: AllCategory){
+        fun bind(categoryItem: AllCategory) {
             movieItemAdapter = MovieItemAdapter(categoryItem.movieItems)
             binding.mrCategoryTitle.text = categoryItem.categoryTitle
-            movieItemAdapter?.stateRestorationPolicy= StateRestorationPolicy.PREVENT_WHEN_EMPTY
-            binding.mrMovieRecyclerView.layoutManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
+            movieItemAdapter?.stateRestorationPolicy = StateRestorationPolicy.PREVENT_WHEN_EMPTY
+            binding.mrMovieRecyclerView.layoutManager =
+                LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
             binding.mrMovieRecyclerView.adapter = movieItemAdapter
             binding.mrMovieRecyclerView.hasFixedSize()
         }
     }
 
 
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainRecyclerAdapter.MainViewHolder {
-        return MainViewHolder(MovieRecyclerRowItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): MainRecyclerAdapter.MainViewHolder {
+        return MainViewHolder(
+            MovieRecyclerRowItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
     }
 
 
---
+++ MovieHub/app/src/main/java/com/example/moviehub/adapters/WatchListAdapter.kt
@@ -0,0 +1,65 @@
+package com.example.moviehub.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.example.moviehub.databinding.WatchListItemBinding
+import com.example.moviehub.models.MediaBody
+
+class WatchListAdapter(
+    private val watchList: ArrayList<MediaBody>
+) :
+    RecyclerView.Adapter<WatchListAdapter.WatchListViewHolder>() {
+
+    inner class WatchListViewHolder(val binding: WatchListItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(watchListItem: MediaBody) {
+            var genres = "Genres: "
+            for (item in watchListItem.genres) {
+                genres = "$genres$item, "
+            }
+
+            binding.wlMovieTitle.text = watchListItem.title
+            binding.wlMovieGenre.text = genres
+            binding.wlImage.load(watchListItem.poster_path)
+
+            if (watchListItem.adult) {
+                binding.wlAdult.visibility = View.VISIBLE
+            } else {
+                binding.wlAdult.visibility = View.GONE
+            }
+
+            if (binding.checkBox.isChecked) {
+                // What happens when checked
+            } else {
+                //otherwise?
+            }
+
+
+        }
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): WatchListAdapter.WatchListViewHolder {
+        return WatchListViewHolder(
+            WatchListItemBinding.inflate(
+                LayoutInflater.from(
+                    parent.context
+                ), parent, false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: WatchListAdapter.WatchListViewHolder, position: Int) {
+        val watchListItem = watchList[position]
+        holder.bind(watchListItem)
+    }
+
+    override fun getItemCount(): Int = watchList.size
+
+}
